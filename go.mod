0a7877cb3ddf7ecc162720036caa663344f47549
Niels Stockfleth <nyls@aol.com>
2025-04-04 18:00:47 +0200

    Copy nginx access log entries into sqlite database

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
 module nylssoft/goaccesslog
 
-go 1.23.2
+go 1.24.2
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.26 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"crypto/md5"
 	"database/sql"
 	"encoding/hex"
-	"errors"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -13,7 +11,8 @@ import (
 	"time"
 	"unicode"
 
-	_ "github.com/lib/pq"
+	"github.com/fsnotify/fsnotify"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type LogLine struct {
@@ -30,181 +29,128 @@ type LogLine struct {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: goaccesslog [add <nginx-logfile-name>...] | [clear] | [analyse] | [view]")
-		os.Exit(-1)
-	}
-	db, err := connectDatabase()
+	ticker := time.NewTicker(60 * time.Second)
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
+	}
+	defer watcher.Close()
+	logDir := "/var/log/nginx"
+	logFile := logDir + "/access.log"
+	go func() {
+		update := false
+		var lastTimeLocal time.Time
+		for {
+			select {
+			case event := <-watcher.Events:
+				if !update && event.Has(fsnotify.Write) && event.Name == logFile {
+					update = true
+					log.Printf("INFO: Detected modified log file '%s'. Update database on next schedule.\n", logFile)
+				}
+			case <-ticker.C:
+				if update {
+					update = false
+					lastTimeLocal, err = updateDatabase(logFile, lastTimeLocal)
+					if err != nil {
+						log.Printf("ERROR: Failed to update database: %s.\n", err.Error())
+					}
+				}
+			case err := <-watcher.Errors:
+				log.Printf("ERROR: Failed to watch directory '%s'. %s\n", logDir, err.Error())
+			}
+		}
+	}()
+	err = watcher.Add(logDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("INFO: Waiting for changes in log file '%s'.\n", logFile)
+	<-make(chan struct{})
+}
+
+func updateDatabase(fileName string, lastTimeLocal time.Time) (time.Time, error) {
+	db, err := prepareDatabase()
+	if err != nil {
+		return lastTimeLocal, err
 	}
 	defer db.Close()
-	switch os.Args[1] {
-	case "add":
-		add(db, os.Args[2:])
-	case "clear":
-		clear(db)
-	case "analyse":
-		analyse(db)
-	case "view":
-		viewBlockedIps(db)
-	}
-}
-
-func analyse(db *sql.DB) {
-	_, err := db.Exec("delete from blockedips")
-	if err != nil {
-		log.Fatal(err)
-	}
-	findSuspiciousRequests(db)
-}
-
-func clear(db *sql.DB) {
-	_, err := db.Exec("delete from accesslog")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = db.Exec("delete from blockedips")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func add(db *sql.DB, fileNames []string) {
 	insertStmt, err := db.Prepare("INSERT INTO accesslog (remote_addr,time_local,request_method,request_uri,request_protocol,request_length,request_time,status,bytes_sent,user_agent,hash) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)")
 	if err != nil {
-		log.Fatal(err)
+		return lastTimeLocal, err
 	}
 	defer insertStmt.Close()
 	hashStmt, err := db.Prepare("SELECT 1 FROM accesslog WHERE hash=$1")
 	if err != nil {
-		log.Fatal(err)
+		return lastTimeLocal, err
 	}
 	defer hashStmt.Close()
-	for _, fileName := range fileNames {
-		err = insertAccessLogFile(insertStmt, hashStmt, fileName)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func viewBlockedIps(db *sql.DB) {
-	rows, err := db.Query(
-		"select ip from blockedips order by ip asc")
+	lastTimeLocal, err = processLogFile(insertStmt, hashStmt, fileName, lastTimeLocal)
 	if err != nil {
-		log.Fatal(err)
+		return lastTimeLocal, err
 	}
-	for rows.Next() {
-		var ip string
-		err = rows.Scan(&ip)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(ip)
-	}
+	return lastTimeLocal, nil
 }
 
-func findSuspiciousRequests(db *sql.DB) {
-	// find requests from IP addresses that failed and used unexpected request methods or took more than 5000ms
-	rows, err := db.Query(
-		"select remote_addr,request_method,request_uri,request_protocol,request_time,status " +
-			"from accesslog where status >= 400 and (request_method not in ('GET','POST','PUT','OPTIONS','PRI','HEAD','DELETE','CONNECT','TRACE','PATCH') or request_time > 5000) and " +
-			"remote_addr not in (select ip from blockedips)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	blockips := make(map[string]bool)
-	for rows.Next() {
-		var remote_addr, request_method, request_uri, request_protocol string
-		var request_time, status int
-		err = rows.Scan(&remote_addr, &request_method, &request_uri, &request_protocol, &request_time, &status)
+func prepareDatabase() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./accesslog.db")
+	if err == nil {
+		stmt := `CREATE TABLE IF NOT EXISTS accesslog (
+			remote_addr TEXT,
+			time_local TIMESTAMP,
+			request_method TEXT,
+			request_uri TEXT,
+			request_protocol TEXT,
+			request_length INTEGER,
+			request_time INTEGER,
+			status INTEGER,
+			bytes_sent INTEGER,
+			user_agent TEXT,
+			hash TEXT)`
+		_, err = db.Exec(stmt)
+		if err == nil {
+			stmt = "CREATE INDEX IF NOT EXISTS accesslog_hash_idx ON accesslog (hash)"
+			_, err = db.Exec(stmt)
+		}
 		if err != nil {
-			log.Fatal(err)
-		}
-		if !blockips[remote_addr] && !isValidUri(request_uri) {
-			fmt.Println("Block IP", remote_addr, request_method, request_uri, request_protocol, request_time, status)
-			blockips[remote_addr] = true
+			db.Close()
 		}
 	}
-	// find requests from IP addresses that failed more than 100 times or redirects more than 100 times
-	rows, err = db.Query(
-		"select cnt,remote_addr,request_uri,status from " +
-			"(select count(remote_addr) as cnt,remote_addr,request_uri,status from accesslog where (status >= 400 or status = 301) " +
-			"group by remote_addr,request_uri,status) as tmp where " +
-			"tmp.cnt > 100 and tmp.remote_addr not in (select ip from blockedips)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for rows.Next() {
-		var remote_addr, request_uri string
-		var cnt, status int
-		err = rows.Scan(&cnt, &remote_addr, &request_uri, &status)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !blockips[remote_addr] && !isValidUri(request_uri) {
-			fmt.Println("Block IP", remote_addr, request_uri, status, cnt)
-			blockips[remote_addr] = true
-		}
-	}
-	if len(blockips) > 0 {
-		stmt, err := db.Prepare("INSERT INTO blockedips (ip) VALUES ($1)")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer stmt.Close()
-		for ip := range blockips {
-			_, err = stmt.Exec(ip)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+	return db, err
 }
 
-func isValidUri(uri string) bool {
-	test := strings.ToLower(uri)
-	if test == "/" {
-		return true
-	}
-	patterns := []string{"api", "arkanoid", "backgammon", "chess", "contacts", "diary", "documents", "index", "makeadate", "notes", "pwdman", "password", "skat", "slideshow", "tetris", "usermgmt", "view", "webpack"}
-	for _, p := range patterns {
-		if strings.Contains(test, p) {
-			return true
-		}
-	}
-	return false
-}
-
-func insertAccessLogFile(insertStmt, hashStmt *sql.Stmt, fileName string) error {
-	fmt.Printf("Process log file '%s'...\n", fileName)
+func processLogFile(insertStmt, hashStmt *sql.Stmt, fileName string, lastTimeLocal time.Time) (time.Time, error) {
+	log.Printf("INFO: Process log entries in log file '%s'. Last processed log entry: %s.\n", fileName, lastTimeLocal)
 	bytes, err := os.ReadFile(fileName)
 	if err != nil {
-		return err
+		return lastTimeLocal, err
 	}
 	data := string(bytes)
 	data = strings.ReplaceAll(data, "\t", "")
 	data = strings.ReplaceAll(data, "\r", "")
 	lines := strings.Split(data, "\n")
-	insertCnt, skipCnt := 0, 0
+	insertCnt, skipCnt, errCnt := 0, 0, 0
 	for _, line := range lines {
-		hash := hashLine(line)
-		logLine, ok := parseLogLine(line)
-		if ok {
-			skip, err := insertLogLine(insertStmt, hashStmt, logLine, hash)
+		logLine, err := parseLogLine(line)
+		if err != nil {
+			log.Printf("ERROR: Failed to process log line: '%s'. %s\n", line, err.Error())
+			continue
+		}
+		if logLine.time_local.Compare(lastTimeLocal) >= 0 {
+			skipped, err := insertLogLine(insertStmt, hashStmt, logLine, hashLine(line))
 			if err != nil {
-				return err
-			}
-			if skip {
+				errCnt++
+			} else if skipped {
 				skipCnt++
 			} else {
 				insertCnt++
 			}
+			lastTimeLocal = logLine.time_local
 		}
 	}
-	fmt.Printf("Processed %d lines in log file. Inserted %d row(s). Skipped %d row(s).\n", insertCnt+skipCnt, insertCnt, skipCnt)
-	return nil
+	if insertCnt > 0 || skipCnt > 0 || errCnt > 0 {
+		log.Printf("INFO: Inserted %d log lines. Skipped %d log lines. Errors occurred in %d log lines.\n", insertCnt, skipCnt, errCnt)
+	}
+	return lastTimeLocal, nil
 }
 
 func hashLine(line string) string {
@@ -213,75 +159,15 @@ func hashLine(line string) string {
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-func connectDatabase() (*sql.DB, error) {
-	con := os.Getenv("DB_DATASOURCE")
-	if len(con) == 0 {
-		return nil, errors.New("MISSING_ENV_DB_DATASOURCE")
-	}
-	db, err := sql.Open("postgres", con)
-	if err != nil {
-		return nil, err
-	}
-	err = db.Ping()
-	if err != nil {
-		db.Close()
-		return nil, err
-	}
-	query := `create table if not exists accesslog (
-			remote_addr varchar(32),
-			time_local timestamptz,
-			request_method varchar(32),
-			request_uri varchar,
-			request_protocol varchar(32),
-			request_length int,
-			request_time int,
-			status int,
-			bytes_sent int,
-			user_agent varchar,
-			hash varchar(100)
-		)`
-	_, err = db.Exec(query)
-	if err != nil {
-		db.Close()
-		return nil, err
-	}
-	query = "create index if not exists accesslog_hash_idx on accesslog (hash)"
-	_, err = db.Exec(query)
-	if err != nil {
-		db.Close()
-		return nil, err
-	}
-	query = `create table if not exists blockedips (ip varchar(32))`
-	_, err = db.Exec(query)
-	if err != nil {
-		db.Close()
-		return nil, err
-	}
-	return db, nil
-}
-
-func insertLogLine(insertStmt, hashStmt *sql.Stmt, logLine LogLine, hash string) (bool, error) {
-	rows, err := hashStmt.Query(hash)
-	if err != nil {
-		return false, err
-	}
-	defer rows.Close()
-	skipped := true
-	if !rows.Next() {
-		_, err = insertStmt.Exec(logLine.remote_addr, logLine.time_local, logLine.request_method, logLine.request_uri, logLine.request_protocol, logLine.request_length, logLine.request_time, logLine.status, logLine.bytes_sent, logLine.user_agent, hash)
-		if err != nil {
-			return false, err
-		}
-		skipped = false
-	}
-	return skipped, nil
-}
-
-func parseLogLine(line string) (LogLine, bool) {
+func parseLogLine(line string) (LogLine, error) {
+	var err error
 	logLine := LogLine{}
 	logLine.remote_addr, line = parseIpAddress(line)
 	if len(logLine.remote_addr) > 0 {
-		_, line = parseDate(line)
+		_, line, err = parseDate(line)
+		if err != nil {
+			return logLine, err
+		}
 		msec, line := parseMsec(line)
 		logLine.time_local = time.UnixMilli(msec)
 		logLine.request_uri, line = parseRequest(line)
@@ -300,9 +186,9 @@ func parseLogLine(line string) (LogLine, bool) {
 		logLine.bytes_sent, line = parseInt(line)
 		logLine.request_time, line = parseDuration(line)
 		logLine.user_agent, _ = parseEnv(line)
-		return logLine, true
+		return logLine, nil
 	}
-	return logLine, false
+	return logLine, nil
 }
 
 func parseIpAddress(line string) (string, string) {
@@ -316,15 +202,12 @@ func parseIpAddress(line string) (string, string) {
 	return "", ""
 }
 
-func parseDate(line string) (time.Time, string) {
+func parseDate(line string) (time.Time, string, error) {
 	str, rest := extractString(line, '[', ']')
 	arr := strings.Split(str, " ")
 	datestr := strings.Replace(arr[0], ":", " ", 1)
 	t, err := time.Parse("02/Jan/2006 15:04:05", datestr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return t, rest
+	return t, rest, err
 }
 
 func parseRequest(line string) (string, string) {
@@ -390,4 +273,21 @@ func extractString(line string, bracketStart, bracketEnd rune) (string, string) 
 		}
 	}
 	return "", ""
+}
+
+func insertLogLine(insertStmt, hashStmt *sql.Stmt, logLine LogLine, hash string) (bool, error) {
+	rows, err := hashStmt.Query(hash)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+	skipped := true
+	if !rows.Next() {
+		_, err = insertStmt.Exec(logLine.remote_addr, logLine.time_local, logLine.request_method, logLine.request_uri, logLine.request_protocol, logLine.request_length, logLine.request_time, logLine.status, logLine.bytes_sent, logLine.user_agent, hash)
+		if err != nil {
+			return false, err
+		}
+		skipped = false
+	}
+	return skipped, nil
 }
